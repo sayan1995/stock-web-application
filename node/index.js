74a004e2a738604c9d9c3cc4ff1c0413ec3c2942
Sayan Guha <sayan@Sayans-MacBook-Pro.local>
2019-11-25 18:59:54 -0600

    added server

--- node/index.js
+++ node/index.js
@@ -15,6 +15,16 @@ app.use(function(req, res, next) {
     next();
 });
 
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
+
+
 /*MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     console.log("Database created!");
